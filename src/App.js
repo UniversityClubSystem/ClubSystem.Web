8eac06b81fecf45f59b7ae4d4dd736fbb528533a
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-11-07 14:17:17 +0300

    CssBaseline #1 Closed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Home from './components/Home/Home';
 
 class App extends React.Component {
   render() {
     return(
-      <BrowserRouter>
-        <Route path="/" component={Home} />
-      </BrowserRouter>
+      <React.Fragment>
+        <CssBaseline/>
+        <BrowserRouter>
+          <Route path="/" component={Home}/>
+        </BrowserRouter>
+      </React.Fragment>
+
     )
   }
 }
