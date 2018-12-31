c4823732ca186931d870ba6c4e4e60155247a0d0
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-12-31 23:18:04 +0300

    simple post

--- src/components/Posts/FullPost/FullPost.jsx
+++ src/components/Posts/FullPost/FullPost.jsx
@@ -5,19 +5,26 @@ import styles from './fullPost.module.css';
 
 const FullPost = (props) => {
   const { location } = props;
+  const { post } = location.state;
   console.log(location.pathname);
+  console.log('props:', props);
+
+  // TODO: make util function for date convert
+  post.createdDate = new Date(Date.parse(post.createdDate));
+  const date = `${post.createdDate.getFullYear()}/${(post.createdDate.getMonth() + 1)}/${(post.createdDate.getUTCDate())}`;
+
   return (
     <div className={styles.container}>
       <div className="card text-center">
         <div className="card-header">
-          Featured
+          {post.clubName}
         </div>
         <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <h5 className="card-title">{post.title}</h5>
+          <p className="card-text">{post.content}</p>
         </div>
         <div className="card-footer text-muted">
-          2 days ago
+          {date}
         </div>
       </div>
     </div>
@@ -25,7 +32,18 @@ const FullPost = (props) => {
 };
 
 FullPost.propTypes = {
-  location: PropTypes.object.isRequired
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      post: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        clubId: PropTypes.string.isRequired,
+        clubName: PropTypes.string.isRequired,
+        createdDate: PropTypes.instanceOf(Date).isRequired,
+        id: PropTypes.string.isRequired
+      }).isRequired
+    }).isRequired
+  }).isRequired,
 };
 
 export default FullPost;
--- src/components/Posts/SimplePost/SimplePost.jsx
+++ src/components/Posts/SimplePost/SimplePost.jsx
@@ -7,33 +7,42 @@ import classNames from 'classnames';
 import styles from './simplePost.module.css';
 
 const SimplePost = (props) => {
-  function postClickHandler(id, event) {
-    if (event) {
-      if (event.key === 'Enter') {
-        props.history.push(`/post/${id}`);
-      }
-    } else {
-      props.history.push(`/post/${id}`);
-    }
-  }
-
+  const { post } = props;
   const postStyle = styles.post;
   const gridBox = classNames('col-xs-12 card bg-light m-3', postStyle);
 
-  const { post } = props;
+  function postClickHandler(id, event) {
+    if (event) {
+      if (event.key === 'Enter') {
+        props.history.push({
+          pathname: `/post/${id}`,
+          state: { post }
+        });
+      }
+    } else {
+      props.history.push({
+        pathname: `/post/${id}`,
+        state: { post }
+      });
+    }
+  }
+
   post.createdDate = new Date(Date.parse(post.createdDate));
   const date = `${post.createdDate.getFullYear()}/${(post.createdDate.getMonth() + 1)}/${(post.createdDate.getUTCDate())}`;
   return (
     <div role="button" tabIndex={0} onKeyPress={event => postClickHandler(post.id, event)} onClick={() => postClickHandler(post.id)} className={gridBox}>
       <div className="card-body">
         <h5 className="card-title">
-          {post.createdBy}
+          {post.title}
+        </h5>
+        <h5 className="card-title">
+          {post.clubName}
         </h5>
         <h6 className="card-subtitle mb-2 text-muted">
           {date}
         </h6>
         <p className="card-text">
-          {post.text}
+          {post.content}
         </p>
       </div>
     </div>
