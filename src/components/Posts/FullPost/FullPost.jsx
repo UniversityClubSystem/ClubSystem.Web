3f918bda33d685c72301d9255da58de7ec7ccd71
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2019-02-02 00:11:18 +0300

    DeletePost feature implemented.
    Signed-off-by: Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>

--- src/components/Posts/FullPost/FullPost.jsx
+++ src/components/Posts/FullPost/FullPost.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import axios from 'axios';
+
+import { Button } from '@material-ui/core';
+
 import styles from './fullPost.module.css';
 
 const FullPost = (props) => {
   const { location } = props;
   const { post } = location.state;
-  console.log(location.pathname);
-  console.log('props:', props);
+  const token = localStorage.getItem('token');
 
   // TODO: make util function for date convert
   post.createdDate = new Date(Date.parse(post.createdDate));
   const date = `${post.createdDate.getFullYear()}/${(post.createdDate.getMonth() + 1)}/${(post.createdDate.getUTCDate())}`;
 
+  function onDelete(id) {
+    if (id) {
+      axios
+        .delete(`/api/post/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+        .then(() => {
+          alert('Deleted.');
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <div className="card text-center">
+      <div className="card text-center mt-5">
         <div className="card-header">
           {post.clubName}
         </div>
         <div className="card-body">
           <h5 className="card-title">{post.title}</h5>
           <p className="card-text">{post.content}</p>
+          <Button className={styles.deleteButtton} variant="contained" color="secondary" onClick={() => onDelete(post.id)}>
+            Delete
+          </Button>
         </div>
         <div className="card-footer text-muted">
           {date}
