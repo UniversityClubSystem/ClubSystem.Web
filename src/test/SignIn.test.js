359c92b0deef31c5c5cb22abaf390ef014d97e2e
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2019-02-05 23:48:47 +0300

    tests init

---
+++ src/test/SignIn.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SignIn from '../components/SignIn/SignIn';
+
+describe('SignIn', () => {
+  it('should render correctly', () => {
+    const component = shallow(<SignIn />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
